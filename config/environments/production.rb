aa02da7cf3b35b81f19dd8a96fd0a1152868a5f5
Saurabh Satralkar <64680367+saurabhsatralkar21@users.noreply.github.com>
2023-02-12 13:23:26 +0100

    Corrections made

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-<<<<<<< HEAD
   config.action_mailer.delivery_method = :letter_opener_web
-=======
-  #config.action_mailer.delivery_method = :letter_opener_web
->>>>>>> c320cb6 (Completed)
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
